c060db0e530dc2b76cbcad63edee8678ead3a0ea
Dan4ik105 <60137868+Dan4ik105@users.noreply.github.com>
2020-01-24 15:29:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active',
+        clickable: true,
+      },
+});
